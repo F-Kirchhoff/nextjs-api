388d0a535cb9bca5a8f6d065aa63899aeb29c176
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-05-09 15:07:56 +0200

    Add starter code

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -2,7 +2,6 @@ import { users } from "../../../db/users";
 
 export default function handler(req, res) {
   if (req.method === "GET") {
-    res.status(200).json(users);
     return;
   }
   res.status(403).send("Error: request method not allowed.");
